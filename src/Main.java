22b6a4fc970d9bbac23648de0e93760feba7481d
Emanuel <nuill.exe@gmail.com>
2025-05-01 23:00:59 -0300

    Adding class AccountBank and CurrentAccount

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,17 @@
 import java.util.Scanner;
 
-public class Main {
+class BankAccount{
+    private String name;
+    protected double saldo;
+    protected String typeAccount;
 
-    public static void main(String[] args) {
+    public double typeNumbersDouble(Scanner read){
 
-        Scanner read = new Scanner(System.in);
-        String nome;
-        String typeAccount = "Corrente";
-        double saldo;
-        String opcao = "0";
-
-        System.out.println("Vamos criar sua conta Corrente no banco Java123. Para isso preciso que voce faça um cadastro");
-        System.out.println("Digite seu nome");
-        nome=read.nextLine();
-        System.out.println("Digite o valor do seu primeiro saldo");
-
+        double value=0;
         while(true){
             if(read.hasNextDouble()){//Verify if the value typed on terminal is a number
-                saldo=read.nextFloat();
-                if(saldo<1){
+                value=read.nextDouble();
+                if(value<1){
                     System.out.println("Valor digitado é menor do que 0. Digite outro valor");
                 }else{
                     break;
@@ -29,21 +22,73 @@ public class Main {
             }
         }
 
+        return value;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+
+
+    public void sacar(Scanner read){
+        System.out.println("Escreva o valor a ser sacado");
+        saldo-=typeNumbersDouble(read);
+    }
+
+    public void depositar(Scanner read){
+        System.out.println("Escreva o valor a ser depositado");
+        saldo+=typeNumbersDouble(read);
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+}
+
+class CurrentAccount extends BankAccount{
+    CurrentAccount(){
+        typeAccount = "Corrente";
+    }
+
+    public String getTypeAccount(){
+        return typeAccount;
+    }
+    //            System.out.println("\n\n*********");
+//            System.out.println("Dados iniciais do cliente");
+//            System.out.println("Nome: " + conta.getName());
+//            System.out.println("Tipo de conta: " + conta.getTypeAccount());
+//            System.out.println("Saldo: " + conta.getSaldo());
+//            System.out.println("********* \n\n");
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        String opcao = "0";
+
+        CurrentAccount conta=new CurrentAccount();
+        System.out.println("Vamos criar sua conta Corrente no banco Java123. Para isso preciso que voce faça um cadastro");
+        System.out.println("Digite seu nome");
+        conta.setName(read.nextLine());
+        System.out.println("Digite o valor do seu primeiro deposito");
+        conta.depositar(read);
+
         System.out.println("Pronto! Cadastro realizado com sucesso! Agora voce deseja realizar alguma operação?");
 
         String menu = """
             Operações
-            1-Transferência
-            2-Receber
-            3-Encerrar sessão
+            1-Ver saldo
+            2-Sacar
+            3-Deposito
+            4-Encerrar sessão
             """;
-        while (!opcao.equals("3")) {
-            System.out.println("\n\n*********");
-            System.out.println("Dados iniciais do cliente");
-            System.out.println("Nome: " + nome);
-            System.out.println("Tipo de conta: " + typeAccount);
-            System.out.println("Saldo: " + saldo);
-            System.out.println("********* \n\n");
+
+        while (!opcao.equals("4")) {
+
 
             System.out.println(menu);
 
@@ -51,15 +96,15 @@ public class Main {
             switch (opcao) {
 
                 case "1":
-                    System.out.println("Digite valor a ser transferido: ");
-                    int valorTransferido = Integer.parseInt(read.nextLine());
-                    saldo -= valorTransferido;
+                    System.out.println("O seu saldo é de "+conta.getSaldo());
                     break;
 
                 case "2":
-                    System.out.println("Digite valor a ser recebido: ");
-                    int valorRecebido = Integer.parseInt(read.nextLine());
-                    saldo += valorRecebido;
+                    conta.sacar(read);
+                    break;
+
+                case "3":
+                    conta.depositar(read);
                     break;
 
                 default:
